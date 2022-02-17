7b37b1d087bc2e41ad2a7f61544a8112f1f43a29
matiascalvar <matiasncalvar@gmail.com>
2022-02-17 16:30:07 -0300

    fix backend url

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import Todo from "./components/Todo/Todo";
 import Folder from "./components/Folder/Folder";
 import './App.css';
 
+export const URL = "http://localhost:3001";
+// http://localhost:3001
+// https://todos-nestjs.herokuapp.com
+
 function App() {
   return (
     <Router>
